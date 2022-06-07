cb603c5ac94eeeea4a3b44d7d42a299180ce0bfd
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-07 17:48:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+const chai = 'chai';
+const expect = chai.expect;
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply);
+let random = Math.ceil(Math.random() * 5);
+console.log(random);
+//modulos
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod)
+    //maximum
+let max = Math.max(5, 7, 9, 20, 5);
+console.log(max);
